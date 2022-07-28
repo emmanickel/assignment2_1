080a5580345d84eb2bdba4193837bde73769030a
enickel <emmajanenickel@gmail.com>
2022-07-28 13:09:50 +1000

    Add selectFn to index.js

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 // Access prompt-sync package for user input:
 const prompt = require('prompt-sync')();
 
+// Call function to operate menu:
+selectFn();
+
 // Function to display menu to user and get selection:
 function dispMenu(){
-    console.log("==================================================================");
-    console.log("                            MusoPlan                       ");
-    console.log("==================================================================");
-    console.log("1. Create a musician");
-    console.log("2. Create a troupe");
-    console.log("3. Add a musician to a troupe");
-    console.log("4. Provide a summary description of a troupe");
-    console.log("5. Provide a detailed description of a troupe")
-    console.log("6. Provide a detailed description of a troupe")
-    console.log("7. Read a list of troupe names to be populated from a file")
-    console.log("8. Write a list of the detailed descriptions for all troupes to a given filename")
-    console.log("9. Exit")
-    let selection = parseInt(prompt("Please enter your selection [1, 2, 3, 4, 5, 6, 7, 8, 9]: "));
+    console.log("=========================================");
+    console.log("|               MusoPlan                |");       
+    console.log("|=======================================|");
+    console.log("|     Please select from one of the     |");
+    console.log("|           following options           |");
+    console.log("|        -----------------------        |");
+    console.log("|                                       |");
+    console.log("| 1. Create a musician                  |");
+    console.log("| 2. Create a troupe                    |");
+    console.log("| 3. Add a musician to a troupe.        |");
+    console.log("| 4. Provide a summary description of a |");
+    console.log("|    troupe                             |");
+    console.log("| 5. Provide a detailed description of  |");
+    console.log("|    a troupe                           |");
+    console.log("| 6. Calculate cost of deploying the    |");
+    console.log("|    troupe for a number of hours       |");
+    console.log("| 7. Read a list of troupe names to be  |");
+    console.log("|    populated from a file              |");
+    console.log("| 8. Write a list of the detailed       |");
+    console.log("|    descriptions for all troupes to    |");
+    console.log("|    given file name                    |");
+    console.log("| 9. Exit                               |");
+    console.log("|_______________________________________|");
+    let selection = (prompt("Please enter your selection [1, 2, 3, 4, 5, 6, 7, 8, 9]: "));
     return selection;
 }
-dispMenu();
+
+function selectFn(){
+    let fn;
+    while(true){
+        fn = parseInt(dispMenu());
+        switch(fn){
+            case 1:
+                console.log("You have selected option 1");
+                continue;
+            case 2:
+                console.log("You have selected option 2");
+                continue;
+            case 3:
+                console.log("You have selected option 3");
+                continue;
+            case 4:
+                console.log("You have selected option 4");
+                continue;
+            case 5:
+                console.log("You have selected option 5");
+                continue;
+            case 6:
+                console.log("You have selected option 6");
+                continue;
+            case 7:
+                console.log("You have selected option 7");
+                continue;
+            case 8:
+                console.log("You have selected option 8");
+                continue;
+            case 9:
+                console.log("Goodbye");
+                break; 
+        }
+        break;
+    }
+}
