85a4da04457c5a9892388c9dc6a7356da26638e9
enickel <emmajanenickel@gmail.com>
2022-07-28 15:14:57 +1000

    Add Guitarist class to Guitarist.js

---
+++ Guitarist.js
@@ -0,0 +1,15 @@
+// Import Student class
+const {Musician} = require('./Musician.js');
+
+class Guitarist extends Musician{
+    constructor(mInst, mName, yrsPlaying, hrlyRate){
+        super(mInst, mName, yrsPlaying, hrlyRate);
+    }
+
+    giveInterestingFact(){
+        console.log("The more strings you have, the better you are");
+        return;
+    }
+}
+
+module.exports = {Guitarist}
