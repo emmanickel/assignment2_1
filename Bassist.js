84f2ed671e30f2f269affa6e9b86cef05a4b3228
enickel <emmajanenickel@gmail.com>
2022-07-30 11:46:59 +1000

    Modify Bassist class in Bassist.js

--- Bassist.js
+++ Bassist.js
@@ -5,7 +5,10 @@ class Bassist extends Musician{
     constructor(mInst, mName, yrsPlaying, hrlyRate){
         super(mInst, mName, yrsPlaying, hrlyRate);
     }
-
+    
+    // The giveInsterestingFact() method logs to the console the interesting fact specific
+    // to the instrumentalist
+    // It has no input parameters
     giveInterestingFact(){
         console.log("Everyone loves a bassist");
         return;
