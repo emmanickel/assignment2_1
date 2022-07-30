f01e5c20e62b24229877287123f62edfb0691a82
enickel <emmajanenickel@gmail.com>
2022-07-30 11:47:28 +1000

    Modify Guitarist class in Guitarist.js

--- Guitarist.js
+++ Guitarist.js
@@ -6,6 +6,9 @@ class Guitarist extends Musician{
         super(mInst, mName, yrsPlaying, hrlyRate);
     }
 
+    // The giveInsterestingFact() method logs to the console the interesting fact specific
+    // to the instrumentalist
+    // It has no input parameters
     giveInterestingFact(){
         console.log("The more strings you have, the better you are");
         return;
