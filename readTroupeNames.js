0d613a20931bcc4597f81b614c6ffd4727e022ce
enickel <emmajanenickel@gmail.com>
2022-07-30 21:05:31 +1000

    Update readTroupeNames.js

--- readTroupeNames.js
+++ readTroupeNames.js
@@ -1,37 +1,34 @@
-const { Guitarist } = require('./Guitarist');
-const { Bassist } = require('./Bassist');
-const { Percussionist } = require('./Percussionist');
-const { Flautist } = require('./Flautist');
-const { Musician } = require('./Musician');
-const {Troupe} = require('./Troupe');
-
 // Access prompt-sync package for user input:
 const prompt = require('prompt-sync')();
 
 // Access fs package for reading/writing to files:
 const fs = require('fs');
 
+// readTroupeNames function reads the troupe names given in a text file
+// and creates troupes with the given names
+// Accepted parameters = array containing all current Troupe objects (to be added to)
 function readTroupeNames(troupes){
-    let fileName = prompt("Please enter a file name to read: ");
+    // Prompt the user to enter the filename (no extension required):
+    let fileName = prompt("Please enter a file name to read (without the file extension): ");
 
-    // Read text file:
+    // Read text file of given name using readFileSync():
     const troupeNames = fs.readFileSync(`${fileName}.txt`, 'utf8');
-    
-    // Print to console:
-    console.log(troupeNames);
 
-    // Store each line in an array:
+    // Store each line as a value inside the array troupeList:
     const troupeList = troupeNames.split("\r\n");
-    
-    // Print the array:
-    console.log(troupeList);
 
-    // Create troupes with given names:
+    // For loop cycles through each of the new troupe names
+    // In order to create troupes with the given names (by pushing to the troupes
+    // array troupes which contains all Troupe objects):
     for(let i = 0; i < troupeList.length; i++){
         troupes.push({tMembers: [], tName: troupeList[i], mDur: '', tGenre: ''});
     }
-    console.log(troupes);
+        
+    // Print new troupe names to the console:
+    console.log(`\nTroupe names added: \n${troupeNames}\n`);
+
     return;
 }
 
+// Export the function readTroupeNames:
 module.exports = {readTroupeNames}
