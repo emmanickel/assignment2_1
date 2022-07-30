395c7b42b618dc284a5ef6683835ecaf14f5a7bd
enickel <emmajanenickel@gmail.com>
2022-07-30 17:56:50 +1000

    Add getTroupeName function to giveDescriptions.js

--- giveDescriptions.js
+++ giveDescriptions.js
@@ -8,15 +8,13 @@ const {Troupe} = require('./Troupe');
 // Access prompt-sync package for user input:
 const prompt = require('prompt-sync')();
 
-function giveSummaryDesc(troupes, detailed){
-    // Declare index variable
+function getTroupeName(troupes){
     let i;
-
     while(true){
         // Show list of troupe names:
         console.log("The current available troupes are: ");            
-        for(let i=0; i<troupes.length; i++){
-            console.log(`\t- ${troupes[i].tName}`);
+        for(let x=0; x<troupes.length; x++){
+            console.log(`\t- ${troupes[x].tName}`);
         }
         // Get user input:
         let tName = prompt("Please select a troupe from the list above: ");
@@ -24,66 +22,73 @@ function giveSummaryDesc(troupes, detailed){
         // Find troupe in array troupes:
         for(i = 0; i < troupes.length; i++){
             if(tName == troupes[i].tName){
-                // Give title
-                console.log(`\nSummary Description`);
-                console.log(`====================`);
-                // Access troupe name from troupes array:
-                console.log(`Troupe name: ${troupes[i].tName}`);
-                // For loop to display troupe members:
-                console.log(`Members: `);
-                    for(let x = 0; x < troupes[i].tMembers.length; x++){
-                        console.log(`\t- ${troupes[i].tMembers[x].mName}`);
-                    }
-                // Access troupe genre from troupes array:
-                console.log(`Genre: ${troupes[i].tGenre}`);
-                // Access troupe minimum duration from troupes array:
-                console.log(`Minimum duration: ${troupes[i].mDur} hours`);
-                // Access troupe hourly rate by calling calHrlyRate function from Troupe class:
-                console.log(`Hourly rate: $${parseInt(Troupe.prototype.calHrlyRate.call(troupes[i]))}/hr`);
-                if(detailed == true){
-                    return i;
-                }
-                else return;
-
+                return i;
             }
         }
-            // If troupe name doesn't exist, give error:
-            console.log("\nInvalid selection. Please try again.\n");
+        // If troupe name doesn't exist, give error:
+        console.log ("\nInvalid selection. Please try again.\n");
+            break;;
     }
+}
+
+function giveSummaryDesc(troupes, detailed, i){
+    
+    // // Get troupe name (index of troupes array):
+    // let i = getTroupeName(troupes);
+
+    // Store summary description in result variable:
+    
+    // Access troupe name from troupes array:
+     result = (`Troupe name: ${troupes[i].tName}\n`);
+     // For loop to display troupe members:
+     result = result.concat(`Members: \n`);
+         for(let x = 0; x < troupes[i].tMembers.length; x++){
+             result = result.concat(`\t- ${troupes[i].tMembers[x].mName}\n`);
+         }
+     // Access troupe genre from troupes array:
+     result = result.concat(`Genre: ${troupes[i].tGenre}\n`);
+     // Access troupe minimum duration from troupes array:
+     result = result.concat(`Minimum duration: ${troupes[i].mDur} hours\n`);
+     // Access troupe hourly rate by calling calHrlyRate function from Troupe class:
+     result = result.concat(`Hourly rate: $${parseInt(Troupe.prototype.calHrlyRate.call(troupes[i]))}/hr\n\n`);
+     
+     // Add detailed description to result variable by calling the function 
+     // giveDetailedDesc():
+     
+    if(detailed == true){
+        result = result.concat(giveDetailedDesc(troupes, i, result));
+        return result;
+    }
+     else return result;
     }
 
-function giveDetailedDesc(troupes){
-    let i = giveSummaryDesc(troupes, true);
+function giveDetailedDesc(troupes, i, result){
     // Introduce each musician:
     for(let x=0; x<troupes[i].tMembers.length; x++){
-        console.log(troupes[i].tMembers[x]);
-        console.log(`Name: ${troupes[i].tMembers[x].mName}`);
-        console.log(`Instrument: ${troupes[i].tMembers[x].mInst}`);
-        console.log(`Years playing: ${troupes[i].tMembers[x].yrsPlaying}`);
-        console.log(`Hourly rate: ${troupes[i].tMembers[x].hrlyRate}`);
+        result = result.concat(`Name: ${troupes[i].tMembers[x].mName}\nInstrument: ${troupes[i].tMembers[x].mInst}\nYears playing: ${troupes[i].tMembers[x].yrsPlaying}\nHourly rate: ${troupes[i].tMembers[x].hrlyRate}\n`);
         // console.log(`Interesting fact: ${troupes[i].tMembers[x].toUpperCase().prototype.giveInterestingFact.call(troupes[i].tMembers[x])}`);
 
         // Get interesting fact depending on instrument
         switch(troupes[i].tMembers[x].mInst){
             case 'guitarist':
-                return Guitarist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]);
+                result = result.concat(Guitarist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]));
+                return result;
             case 'bassist':
-                return Bassist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]);
+                result = result.concat(Bassist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]));
+                return result;
             case 'percussionist':
-                return Percussionist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]);
+                result = result.concat(Percussionist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]));
+                return result;
             case 'flautist':
-                return Flautist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]);
+                result = result.concat(Flautist.prototype.giveInterestingFact.call(troupes[i].tMembers[x]));
+                return result;
             default:
                 console.log("Instrument unknown");
                 return;
         }
         // console.log(`Interesting fact: ${Musician.prototype.giveInterestingFact.call(troupes[i].tMembers[x])}`);
     }
-    // Get interesting fact depending on instrument
-    // console.log(troupes[i].tMember);
-    // console.log(Musician.prototype.giveInterestingFact.call(troupes[i].t));
-
 }
 
 
-module.exports = {giveSummaryDesc, giveDetailedDesc}
+module.exports = {giveSummaryDesc, giveDetailedDesc, getTroupeName}
