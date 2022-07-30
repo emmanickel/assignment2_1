ba380a39a5f3af234282928d110c94b3cb89a530
enickel <emmajanenickel@gmail.com>
2022-07-30 11:48:43 +1000

    Update Percussionist class in Percussionist.js

---
+++ Percussionist.js
@@ -0,0 +1,18 @@
+// Import Musician Class
+const {Musician} = require('./Musician.js');
+
+class Percussionist extends Musician{
+    constructor(mInst, mName, yrsPlaying, hrlyRate){
+        super(mInst, mName, yrsPlaying, hrlyRate);
+    }
+
+// The giveInsterestingFact() method logs to the console the interesting fact specific
+// to the instrumentalist
+ // It has no input parameters
+    giveInterestingFact(){
+        console.log("Me drum");
+        return;
+    }
+}
+
+module.exports = {Percussionist}
