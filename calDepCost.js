7a80fd35a4eae3898d04ded8442213f5471628c4
enickel <emmajanenickel@gmail.com>
2022-08-02 21:04:51 +1000

    Update rounding in calDepCost function in calDepCost.js

--- calDepCost.js
+++ calDepCost.js
@@ -19,7 +19,7 @@ function calDepCost(troupes){
 
     // Get troupe name from user:
     let tName = prompt("Please select a troupe from the list above: ").trim();
-
+    
     // Check given musician name is valid:
     for(i = 0; i < troupes.length; i++){
         if(tName.toLowerCase() == troupes[i].tName.toLowerCase()){
@@ -28,7 +28,12 @@ function calDepCost(troupes){
                 hours = prompt(`Please enter the number of hours to deploy ${troupes[i].tName} for: `);
                 // If condition confirms given number of hours is greater than or equal to minimum but less than or equal to 3:
                 if(hours >= troupes[i].mDur&& hours <= 3){
-                    console.log( `Cost of deployment of ${troupes[i].tName} for ${hours} hours is: $${hours*Troupe.prototype.calHrlyRate.call(troupes[i])}.\n`);
+                    // Calculate deployment cost:
+                    let depCost = hours*Troupe.prototype.calHrlyRate.call(troupes[i]);
+                    // Round to 2 decimal places:
+                    depCost = Math.round((depCost + Number.EPSILON) * 100) / 100;
+                    // Print to console:
+                    console.log( `Cost of deployment of ${troupes[i].tName} for ${hours} hours is: $${depCost}.\n`);
                     return;
                 }
                 else{
