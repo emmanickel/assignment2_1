dc959c947aa3331c2d56ab2c9c4fb152b07efff6
enickel <emmajanenickel@gmail.com>
2022-08-03 17:26:24 +1000

    Update index.js file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// MusoPlan Entry-Point Page
+// ==========================
+// Emma Nickel
+// --------------------------
+
 // Access prompt-sync package for user input:
 const prompt = require('prompt-sync')();
 
@@ -10,91 +15,77 @@ let giveDescriptions = require('./giveDescriptions.js');
 let calDepCost = require('./calDepCost.js');
 let readTroupeNames = require('./readTroupeNames.js');
 let writeToFile = require('./writeToFile.js');
-const { Guitarist } = require('./Guitarist.js');
-const { Flautist } = require('./Flautist.js');
-const { Percussionist } = require('./Percussionist.js');
-const { Bassist } = require('./Bassist.js');
-const { Troupe } = require('./Troupe.js');
 
 // Create global maps of musicians and troupes:
 const musicians = new Array();
 const troupes = new Array();
 
 //===========================================================================
-// Questions to ask on Monday:
-// - Can names (of musicians and troupes) be numbers?
-//     - isNaN(musicians[i].mName)
-//     - isNaN(troupes[i].tName)
-// - When there are no members of a troupe, should we still print the summary
-// and detailed description?
-// - Allow create a second musician/troupe with the same name?
-// - Case sensitivity on input?
-//===========================================================================
 // FOR TESTING:
 // Create some musicians:
-let g1 = new Guitarist()
-g1.mInst = 'guitarist';
-g1.mName = "emma";
-g1.yrsPlaying = '8';
-g1.hrlyRate = '55';
-musicians.push(g1);
+// let g1 = new Guitarist()
+// g1.mInst = 'guitarist';
+// g1.mName = "emma";
+// g1.yrsPlaying = '8';
+// g1.hrlyRate = '55';
+// musicians.push(g1);
 
-let b1 = new Bassist()
-b1.mInst = 'bassist';
-b1.mName = "lewis";
-b1.yrsPlaying = '2';
-b1.hrlyRate = '75';
-musicians.push(b1);
+// let b1 = new Bassist()
+// b1.mInst = 'bassist';
+// b1.mName = "lewis";
+// b1.yrsPlaying = '2';
+// b1.hrlyRate = '75';
+// musicians.push(b1);
 
-let p1 = new Percussionist()
-p1.mInst = 'percussionist';
-p1.mName = "zac";
-p1.yrsPlaying = '7';
-p1.hrlyRate = '55';
-musicians.push(p1);
+// let p1 = new Percussionist()
+// p1.mInst = 'percussionist';
+// p1.mName = "zac";
+// p1.yrsPlaying = '7';
+// p1.hrlyRate = '55';
+// musicians.push(p1);
 
-let f1 = new Flautist()
-f1.mInst = 'flautist';
-f1.mName = "rebecca";
-f1.yrsPlaying = '9';
-f1.hrlyRate = '95';
-musicians.push(f1);
+// let f1 = new Flautist()
+// f1.mInst = 'flautist';
+// f1.mName = "rebecca";
+// f1.yrsPlaying = '9';
+// f1.hrlyRate = '95';
+// musicians.push(f1);
 
-let p2 = new Percussionist()
-p2.mInst = 'percussionist';
-p2.mName = "shelby";
-p2.yrsPlaying = '5';
-p2.hrlyRate = '75';
-musicians.push(p2);
+// let p2 = new Percussionist()
+// p2.mInst = 'percussionist';
+// p2.mName = "shelby";
+// p2.yrsPlaying = '5';
+// p2.hrlyRate = '75';
+// musicians.push(p2);
 
-let f2 = new Flautist()
-f2.mInst = 'flautist';
-f2.mName = "andrew";
-f2.yrsPlaying = '9';
-f2.hrlyRate = '100';
-musicians.push(f2);
+// let f2 = new Flautist()
+// f2.mInst = 'flautist';
+// f2.mName = "andrew";
+// f2.yrsPlaying = '9';
+// f2.hrlyRate = '100';
+// musicians.push(f2);
 
-// Create some troupes:
-let t1 = new Troupe()
-t1.tMembers = [];
-t1.tName = "sitting down";
-t1.mDur = '2';
-t1.tGenre = 'jazz';
-troupes.push(t1);
+// // Create some troupes:
+// let t1 = new Troupe()
+// t1.tMembers = [];
+// t1.tName = "sitting down";
+// t1.mDur = '2';
+// t1.tGenre = 'jazz';
+// troupes.push(t1);
 
-let t2 = new Troupe()
-t2.tMembers = [];
-t2.tName = "the strokes";
-t2.mDur = '3';
-t2.tGenre = 'rock';
-troupes.push(t2);
+// let t2 = new Troupe()
+// t2.tMembers = [];
+// t2.tName = "the strokes";
+// t2.mDur = '3';
+// t2.tGenre = 'rock';
+// troupes.push(t2);
 
-// Add some musicians to a troupe:
-t1.tMembers.push(g1);
-t1.tMembers.push(b1);
-t1.tMembers.push(p1);
-t1.tMembers.push(f1);
-t1.tMembers.push(p2);
+// // Add some musicians to a troupe:
+// t1.tMembers.push(g1);
+// t1.tMembers.push(b1);
+// t1.tMembers.push(p1);
+// t1.tMembers.push(f1);
+// t1.tMembers.push(p2);
 
 //===========================================================================
 
@@ -119,31 +110,42 @@ function selectFn(){
         fn = parseInt(dispMenu.dispMenu());
         switch(fn){
             case 1:
+                // Clear the menu from the console:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to register a musician.");
-                // Add a musician by calling the regMusician function:
+                // Add a musician by calling the regMusician function and pushing
+                // the Musician object that is returned to the musicians array:
                 musicians.push(regMusician.regMusician(musicians));
                 console.clear();
+                // Successful execution message:
                 console.log("You have successfully created a musician.\n");
                 prompt("Press the enter key to continue.");
                 continue;
             case 2:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to register a troupe.\n");
-                // Add a troupe by calling the regTroupe function:
+                // Add a troupe by calling the regTroupe function and pushing the 
+                // returned Troupe object to the array troupes:
                 troupes.push(regTroupe.regTroupe(troupes));
                 console.clear();
                 console.log("You have successfully created a troupe.");
+                // Successful execution message:
                 prompt("Press the enter key to continue.");
                 continue;
             case 3:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to add a musician to a troupe.\n");
+                // Call dispTroupeMembers function (which calls the addMusicianTroupe
+                // function) to add a musician to a troupe
                 addMusicianTroupe.dispTroupeMembers(musicians, troupes);
                 prompt("Press the enter key to continue.");
                 continue;
             case 4:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to provide a summary description of a troupe.");
                 // Get troupe name (by index) by calling getTroupeName function
                 index = giveDescriptions.getTroupeName(troupes);
@@ -157,6 +159,7 @@ function selectFn(){
                 continue;
             case 5:
                 console.clear();
+                // Successful selection message:    
                 console.log("You have selected to provide a detailed description of a troupe.");
                 // Get troupe name (by index) by calling getTroupeName function
                 index = giveDescriptions.getTroupeName(troupes);
@@ -170,14 +173,16 @@ function selectFn(){
                 continue;
             case 6:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to calculate the cost of deploying the troupe for a number of hours");
                 console.clear();
-                // Get cost of deployment throught the calDepCost function:
+                // Get cost of deployment through calling the calDepCost function:
                 calDepCost.calDepCost(troupes);
                 prompt("Press the enter key to continue.");
                 continue;
             case 7:
                 console.clear();
+                // Successful selection message:    
                 console.log("You have selected to read a list of troupe names to be populated from a file.");
                 console.clear();
                 // Read troupe names from a file using the readTroupeNames function
@@ -186,15 +191,18 @@ function selectFn(){
                 continue;
             case 8:
                 console.clear();
+                // Successful selection message:
                 console.log("You have selected to write a list of the detailed descriptions for all troupes to a given file name.");
                 console.clear();
-                // Write detailed descriptions of all troupes to file of given name:
+                // Write detailed descriptions of all troupes to file of given name
+                // by calling the writeFileDesc function
                 fileName = writeToFile.writeFileDesc(troupes);
                 console.log(`You have successfully written a list of the detailed descriptions for all troupes to a file called "${fileName}.txt".\n`);
                 prompt("Press the enter key to continue.");
                 continue;
             case 9:
                 console.clear();
+                // Goodbye message
                 console.log("Goodbye");
                 // Break out of switch statement if user selects to exit program:
                 break; 
@@ -205,10 +213,7 @@ function selectFn(){
                 prompt("Press the enter key to continue.");
                 continue;
             }
-        // Break out of while loop once switch is complete:
+        // Break out of while loop once switch break occurs:
         break;
     }
 }
-
-
-module.exports = {selectFn}
